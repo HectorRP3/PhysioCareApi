9ea1143070e06abf64d582f960cfd18d264bf8a0
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-05-08 18:34:03 +0200

    Update token

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "X-XSRF-TOKEN",
+      "Authorization",
+      "x-xsrf-token",
+    ],
   })
 );
 app.use(express.json());
