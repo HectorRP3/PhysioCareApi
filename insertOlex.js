6f26ddb479af4888ee58392c137d8843a6d48b57
0l3x <48629513+0l3x@users.noreply.github.com>
2025-05-10 19:20:05 +0200

    Create insertOlex.js

---
+++ insertOlex.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bcrypt = require("bcryptjs");
+const User = require("./models/users");
+
+// Cargar variables de entorno (como DB_URL)
+dotenv.config();
+
+async function insertSingleUser() {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("✅ Conectado a MongoDB");
+
+    const hashedPassword = await bcrypt.hash("1234", 10);
+
+    const newUser = new User({
+        login: "Olex",
+        password: "1234",
+        rol: "admin",
+    });
+
+    const saved = await newUser.save();
+    console.log("✅ Usuario insertado:", saved);
+
+    mongoose.disconnect();
+  } catch (error) {
+    console.error("❌ Error insertando usuario:", error);
+    mongoose.disconnect();
+  }
+}
+
+insertSingleUser();
+
+
+
