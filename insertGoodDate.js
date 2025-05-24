d1aa5b016f7462edb4a3e4a860f2650c969d4713
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-05-24 15:33:49 +0200

    fdsa

--- insertGoodDate.js
+++ insertGoodDate.js
@@ -32,46 +32,46 @@ async function loadData() {
     const savedUsers = await Promise.all(users.map((user) => user.save()));
     console.log("Added users:", savedUsers);
     // Create some patients
-    // const patients = [
-    //   new Patient({
-    //     name: "Pedro",
-    //     surname: "López",
-    //     birthDate: new Date("1985-06-15"),
-    //     address: "Calle Mayor 123, Alicante",
-    //     insuranceNumber: "123406789",
-    //     email: "hectorrodriguezplanelles@gmail.com",
-    //   }),
-    //   new Patient({
-    //     name: "Maria",
-    //     surname: "Pérez",
-    //     birthDate: new Date("1990-09-22"),
-    //     address: "Avenida del Sol 45, Valencia",
-    //     insuranceNumber: "980654321",
-    //     email: "hectorrodriguezplanelles@gmail.com",
-    //   }),
-    //   new Patient({
-    //     name: "Luis",
-    //     surname: "Martínez",
-    //     birthDate: new Date("1975-03-11"),
-    //     address: "Calle de la Luna 89, Alicante",
-    //     insuranceNumber: "456784123",
-    //     email: "hectorrodriguezplanelles@gmail.com",
-    //   }),
-    //   new Patient({
-    //     name: "María",
-    //     surname: "Sanz",
-    //     birthDate: new Date("1992-05-30"),
-    //     address: "Plaza Mayor 22, Valencia",
-    //     insuranceNumber: "321657987",
-    //     email: "hectorrodriguezplanelles@gmail.com",
-    //   }),
-    // ];
+    const patients = [
+      new Patient({
+        name: "Pedro",
+        surname: "López",
+        birthDate: new Date("1985-06-15"),
+        address: "Calle Mayor 123, Alicante",
+        insuranceNumber: "123406789",
+        email: "hectorrodriguezplanelles@gmail.com",
+      }),
+      new Patient({
+        name: "Maria",
+        surname: "Pérez",
+        birthDate: new Date("1990-09-22"),
+        address: "Avenida del Sol 45, Valencia",
+        insuranceNumber: "980654321",
+        email: "hectorrodriguezplanelles@gmail.com",
+      }),
+      new Patient({
+        name: "Luis",
+        surname: "Martínez",
+        birthDate: new Date("1975-03-11"),
+        address: "Calle de la Luna 89, Alicante",
+        insuranceNumber: "456784123",
+        email: "hectorrodriguezplanelles@gmail.com",
+      }),
+      new Patient({
+        name: "María",
+        surname: "Sanz",
+        birthDate: new Date("1992-05-30"),
+        address: "Plaza Mayor 22, Valencia",
+        insuranceNumber: "321657987",
+        email: "hectorrodriguezplanelles@gmail.com",
+      }),
+    ];
 
-    // // Save all patients using Promise.all
-    // const savedPatients = await Promise.all(
-    //   patients.map((patient) => patient.save())
-    // );
-    // console.log("Added patients:", savedPatients);
+    // Save all patients using Promise.all
+    const savedPatients = await Promise.all(
+      patients.map((patient) => patient.save())
+    );
+    console.log("Added patients:", savedPatients);
 
     // Create several physios, at least one for each specialty
     const physios = [
