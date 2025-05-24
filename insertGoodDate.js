c13c610f5e75dc96601cf5513a1cea42723c3d76
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-05-24 15:32:15 +0200

    fdsa
    fdsa

--- insertGoodDate.js
+++ insertGoodDate.js
@@ -10,67 +10,68 @@ async function loadData() {
     // await Physio.deleteMany({});
     // await Record.deleteMany({});
     // await User.deleteMany({});
-
-    // const users = [
-    //   new User({
-    //     login: "olexPhysio",
-    //     password: "1234",
-    //     rol: "physio",
-    //   }),
-    //   new User({
-    //     login: "hectorPhysio",
-    //     password: "1234",
-    //     rol: "physio",
-    //   }),
-    // ];
-    // for (let i = 0; i < users.length; i++) {
-    //   const hashedPassword = await bycrypt.hash(users[i].password, 10);
-    //   users[i].password = hashedPassword;
-    // }
+    await User.delete({ login: "hectorPhysio" });
+    await User.delete({ login: "olexPhysio" });
+    const users = [
+      new User({
+        login: "olexPhysio",
+        password: "1234",
+        rol: "physio",
+      }),
+      new User({
+        login: "hectorPhysio",
+        password: "1234",
+        rol: "physio",
+      }),
+    ];
+    for (let i = 0; i < users.length; i++) {
+      const hashedPassword = await bycrypt.hash(users[i].password, 10);
+      users[i].password = hashedPassword;
+    }
 
     // const savedUsers = await Promise.all(users.map((user) => user.save()));
     // console.log("Added users:", savedUsers);
     // Create some patients
-    const patients = [
-      new Patient({
-        name: "Pedro",
-        surname: "López",
-        birthDate: new Date("1985-06-15"),
-        address: "Calle Mayor 123, Alicante",
-        insuranceNumber: "123406789",
-        email: "hectorrodriguezplanelles@gmail.com",
-      }),
-      new Patient({
-        name: "Maria",
-        surname: "Pérez",
-        birthDate: new Date("1990-09-22"),
-        address: "Avenida del Sol 45, Valencia",
-        insuranceNumber: "980654321",
-        email: "hectorrodriguezplanelles@gmail.com",
-      }),
-      new Patient({
-        name: "Luis",
-        surname: "Martínez",
-        birthDate: new Date("1975-03-11"),
-        address: "Calle de la Luna 89, Alicante",
-        insuranceNumber: "456784123",
-        email: "hectorrodriguezplanelles@gmail.com",
-      }),
-      new Patient({
-        name: "María",
-        surname: "Sanz",
-        birthDate: new Date("1992-05-30"),
-        address: "Plaza Mayor 22, Valencia",
-        insuranceNumber: "321657987",
-        email: "hectorrodriguezplanelles@gmail.com",
-      }),
-    ];
+    // const patients = [
+    //   new Patient({
+    //     name: "Pedro",
+    //     surname: "López",
+    //     birthDate: new Date("1985-06-15"),
+    //     address: "Calle Mayor 123, Alicante",
+    //     insuranceNumber: "123406789",
+    //     email: "hectorrodriguezplanelles@gmail.com",
+    //   }),
+    //   new Patient({
+    //     name: "Maria",
+    //     surname: "Pérez",
+    //     birthDate: new Date("1990-09-22"),
+    //     address: "Avenida del Sol 45, Valencia",
+    //     insuranceNumber: "980654321",
+    //     email: "hectorrodriguezplanelles@gmail.com",
+    //   }),
+    //   new Patient({
+    //     name: "Luis",
+    //     surname: "Martínez",
+    //     birthDate: new Date("1975-03-11"),
+    //     address: "Calle de la Luna 89, Alicante",
+    //     insuranceNumber: "456784123",
+    //     email: "hectorrodriguezplanelles@gmail.com",
+    //   }),
+    //   new Patient({
+    //     name: "María",
+    //     surname: "Sanz",
+    //     birthDate: new Date("1992-05-30"),
+    //     address: "Plaza Mayor 22, Valencia",
+    //     insuranceNumber: "321657987",
+    //     email: "hectorrodriguezplanelles@gmail.com",
+    //   }),
+    // ];
 
-    // Save all patients using Promise.all
-    const savedPatients = await Promise.all(
-      patients.map((patient) => patient.save())
-    );
-    console.log("Added patients:", savedPatients);
+    // // Save all patients using Promise.all
+    // const savedPatients = await Promise.all(
+    //   patients.map((patient) => patient.save())
+    // );
+    // console.log("Added patients:", savedPatients);
 
     // Create several physios, at least one for each specialty
     const physios = [
